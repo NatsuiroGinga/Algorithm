dd3e3a7b3898a27f4afeed59bb628881f9ce0dd8
NatsuiroGinga <duyueyeweiliang@gmail.com>
2022-07-12 11:25:13 +0800

    null

---
+++ Graph/src/edu/sdau/graph/Main.java
@@ -0,0 +1,13 @@
+package edu.sdau.graph;
+
+/**
+ * @author ginga
+ * @version 1.0
+ * @ClassName Main
+ * @Date 12/7/2022 上午11:24
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
